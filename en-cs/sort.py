de8e00f6632a91ddaab833d82aeea38d04b6892e
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2016-11-18 06:52:22 +0000

    A base for multiple-column unicode-aware sort

---
+++ en-cs/sort.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import icu
+import sys
+
+column_count = 5
+line_number = 0
+entries = []
+
+for line in sys.stdin:
+    line_number += 1
+    entry = line.rstrip('\n').split('\t')
+    if len(entry) != column_count:
+        raise RuntimeError('Format error on line {}: "{}"'.format(line_number, line))
+    entries.append(entry)
+
+cs_sort_key = icu.Collator.createInstance(icu.Locale('cs_CZ.UTF-8')).getSortKey
+en_sort_key = icu.Collator.createInstance(icu.Locale('en_US.UTF-8')).getSortKey
+
+def sort_key(entry):
+    return (
+        en_sort_key(entry[0]),
+        cs_sort_key(entry[1]),
+        cs_sort_key(entry[2]),
+        cs_sort_key(entry[3]),
+        cs_sort_key(entry[4]))
+
+entries.sort(key=sort_key)
+
+for entry in entries:
+    print('\t'.join(entry))
