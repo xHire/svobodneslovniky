968ddaaade16fc79de5518818e087de058c4f2b5
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2016-11-19 09:17:07 +0000

    travis integration

--- en-cs/sort.sh
+++ en-cs/sort.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+set -o nounset
+set -o errexit
+
+dict_file="en-cs.txt"
+tmp_file="sort.tmp"
 
 cd `dirname $0`
-LC_COLLATE=cs_CZ.utf8 sort -f -o tmp.sort en-cs.txt
-mv tmp.sort en-cs.txt
+
+./sort.py < $dict_file > $tmp_file
+mv $tmp_file $dict_file
